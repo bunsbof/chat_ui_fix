479a7dd7f5b13c263028226de167ee4b5c0a3244
bunsbof <nguyenminhcuong2000@gmail.com>
2023-01-02 18:38:00 +0700

    #2023/01/02 cuong - initialize tailwind without build in folder

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl underline">Hello World</h1>
+    <div>
+      <h1 className='text-3xl underline'>Hello World</h1>
     </div>
   );
 }
